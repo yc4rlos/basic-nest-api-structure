22af9da9d7b6a5a744a88a4865a09498c5cc4459
Yan Carlos <shadowsilvers12@gmail.com>
2023-01-04 10:51:48 -0300

    Configure database connection for tests

---
+++ test/database/connection.ts
@@ -0,0 +1,50 @@
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { EntityClassOrSchema } from '@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type';
+import { newDb } from 'pg-mem';
+
+export async function providerConnection(entities: EntityClassOrSchema[]): Promise<any[]> {
+    try {
+
+        // Cria o banco em memória
+        const db = newDb({
+            autoCreateForeignKeyIndices: true
+        });
+
+        // Registra as funções faltantes
+        db.public.registerFunction({
+            implementation: () => 'test',
+            name: 'current_database',
+        });
+        db.public.registerFunction({
+            implementation: () => 'version',
+            name: 'version',
+        });
+
+        // Cria a connection Typeorm com as entities fornecidas
+        const connection = await db.adapters.createTypeormConnection({
+            type: 'postgres',
+            entities: [...entities]
+        });
+
+        // Cria as tabelas
+        await connection.synchronize();
+        let providers = [];
+
+        // Para cada entity cria um objeto de provider correnspondente
+        for (let entity of entities) {
+
+            const token = getRepositoryToken(entity as any);
+            const repository = await connection.getRepository(entity);
+
+            providers.push({
+                provide: token,
+                useValue: repository
+            });
+        }
+        // retorna os providers prontos para uso
+        return providers;
+    } catch (err) {
+        throw new Error(`Cant create database, error: ${err}`);
+    }
+
+}
